28be7f74d383bc3926bcee3ec1ce4e54bf4dc87e
RazzleJazzled <robertjgassner@gmail.com>
2020-10-03 13:49:24 -0700

    overhauled files

--- src/App.js
+++ src/App.js
@@ -1,41 +1,93 @@
-import React, { Component } from 'react';
-import API from "./utils/api"
-import './App.css';
-import Table from './components/table'
-import Search from "./components/search"
-
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Table from "./components/Table";
+import "./app.css";
+import API from "./utils/API";
 
 class App extends Component {
   state = {
-    results: []
+    search: "",
+    employees: [],
   };
+
   componentDidMount() {
-    console.log("hi!")
-    API.getUsers()
-      .then(res => this.setState({ results: res.data.results }))
-      .catch(err => console.log(err))
-  };
+    API.getUsers().then(({ data }) => {
+      this.setState({
+        employees: data.results.map((employee) => ({
+          id: employee.login.uuid,
+          name: `${employee.name.first} ${employee.name.last}`,
+          phone: employee.phone,
+          email: employee.email,
+          image: employee.picture.medium,
+          dob: employee.dob.date,
+        })),
+      });
+    });
+  }
 
   handleInputChange = (event) => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
     });
-  }
+  };
+
+  filterEmployees = (employee) => {
+    const { search } = this.state;
+    if (!search) return true;
+
+    for (const key in employee) {
+      if (employee[key].toLowerCase().includes(search.toLowerCase()))
+        return true;
+    }
+
+    return false;
+  };
+
   render() {
+    const { employees } = this.state;
+
     return (
       <div>
-        <Search search={this.state.search}
-          handleInputChange={this.handleInputchange} />
+        <Header />
+        <p>{this.state.search}</p>
+        <Search
+          search={this.state.search}
+          handleInputChange={this.handleInputChange}
+        />
         <div className="container">
           <div className="row">
-            <Table
-              data={this.state.results}
-            />
+            <table className="table table-striped ">
+              <thead>
+                <tr>
+                  <th scope="col">Image</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Phone</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">DOB</th>
+                </tr>
+              </thead>
+              <tbody>
+                {employees.length &&
+                  employees.filter(this.filterEmployees).map((employee) => (
+                    <tr key={employee.id}>
+                      <Table
+                        id={employee.id}
+                        img={employee.image}
+                        name={employee.name}
+                        phone={employee.phone}
+                        email={employee.email}
+                        dob={employee.dob}
+                      />
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
